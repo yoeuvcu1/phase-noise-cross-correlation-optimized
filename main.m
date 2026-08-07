6b24d28cf987da96d2df50e616e3b9633f5002d3
yoeuvcu1 <umer@yaani.com>
2026-08-07 12:55:47 +0300

    Update main.m

--- main.m
+++ main.m
@@ -1,21 +1,24 @@
 function results = main(config)
+% Simülasyonu başlatan giriş noktası.
 
+% Parametre verilmezse varsayılan test ayarları.
 if nargin == 0
-    config.N = 100000;
-    config.fs = 1e6;
-    config.A = 1;
-    config.f0 = 50e3;
-    config.settling_samples = 100;
-    config.lpf_cutoff = 25e3;
-    config.lpf_order = 4;
-    config.phase_rms_dut = 0.2;
-    config.phase_rms_ref1 = 0.05;
-    config.phase_rms_ref2 = 0.05;
-    config.number_of_iterations = 100;
-    config.number_of_log_bins = 50;
-    config.show_plot = true;
+    config.N = 100000;                 % Toplam örnek sayısı
+    config.fs = 1e6;                   % Örnekleme frekansı (Hz)
+    config.A = 1;                      % Taşıyıcı genliği
+    config.f0 = 50e3;                  % Taşıyıcı frekansı (Hz)
+    config.settling_samples = 100;     % LPF geçici bölgesi atılacak örnek sayısı
+    config.lpf_cutoff = 25e3;          % Faz detektörü LPF kesim frekansı (Hz)
+    config.lpf_order = 4;              % Faz detektörü LPF derecesi
+    config.phase_rms_dut = 0.2;        % DUT faz gürültüsünün RMS değeri (rad)
+    config.phase_rms_ref1 = 0.05;      % Referans 1 faz gürültüsü RMS (rad)
+    config.phase_rms_ref2 = 0.05;      % Referans 2 faz gürültüsü RMS (rad)
+    config.number_of_iterations = 100; % Cross-PSD ortalaması için iterasyon
+    config.number_of_log_bins = 50;    % Logaritmik bin sayısı
+    config.show_plot = true;           % Sonuç grafiğini çiz
 end
 
-results = step01_sources(config);
+% Ana simülasyonu çalıştır ve sonuç yapısını döndür.
+results = run_simulation(config);
 
 end
